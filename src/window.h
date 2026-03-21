a6c8db80bae5f10458589162e536bdcc48a6f7bf
MartinOpat <martinopat.opat@gmail.com>
2026-03-21 22:49:02 +0100

    Fix: pixel size was inverted... oops

--- src/window.h
+++ src/window.h
@@ -2,9 +2,11 @@
 
 #include <ncurses.h>
 
-static inline float get_pixel_size_x() { return getmaxx(stdscr) / LINES; }
+static inline float get_pixel_size_x() {
+  return (float)LINES / getmaxx(stdscr);
+}
 
-static inline float get_pixel_size_y() { return getmaxy(stdscr) / COLS; }
+static inline float get_pixel_size_y() { return (float)COLS / getmaxy(stdscr); }
 
 static inline float get_pixel_radius() {
   return (get_pixel_size_x() + get_pixel_size_y()) / 2.0;
